aa3368cb72ac3332f85916a8c7eea604ca1364aa
Nathan <santos.nna@outlook.com>
2022-06-20 22:54:32 +0200

    Project Set Up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingPageComponent } from './new-listing-page/new-listing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/listings', pathMatch: 'full' }, // This line redirects the user from the root page straight to the listings page
+  { path: 'listings', component: ListingsPageComponent, pathMatch: 'full' }, // The property "pathMatch" is set to "full" so the URL match
+  // exactly these terms. This is important because other URLs can share parts of the path (eg.: next line)
+  { path: 'listings/:id', component: ListingDetailPageComponent },
+  { path: 'contact/:id', component: ContactPageComponent },
+  { path: 'edit-listing/:id', component: EditListingPageComponent },
+  { path: 'my-listings', component: MyListingsPageComponent },
+  { path: 'new-listing', component: NewListingPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
