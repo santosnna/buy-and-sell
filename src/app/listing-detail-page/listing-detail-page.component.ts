6987ae557a42d2209f0a702f1b56539e580b7b1d
Nathan <santos.nna@outlook.com>
2022-06-29 20:16:27 +0200

    1.8: Create a Listing Detail Page

--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-detail-page',
   templateUrl: './listing-detail-page.component.html',
-  styleUrls: ['./listing-detail-page.component.css']
+  styleUrls: ['./listing-detail-page.component.css'],
 })
 export class ListingDetailPageComponent implements OnInit {
+  listing: Listing;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute // ActivatedRoute is a provider which allows the page to access value of URL param in current path
+  ) {}
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find((listing) => listing.id === id);
   }
-
 }
